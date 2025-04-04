322af9d1e48073626002688ba6ae9204c3633c5d
Maria Maas <meagmaas@student.che.nl>
2025-04-04 13:57:19 +0200

    Add project files.

---
+++ ProjectGrowthPath.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using ProjectGrowthPath.Domain.Entities;
+
+
+namespace ProjectGrowthPath.Infrastructure.Persistence
+{
+    class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Add any additional configuration here
+        }
+    }
+   
+}
