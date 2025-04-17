6028eb44979628e0348da01cd296baceaeff3442
Maria Maas <meagmaas@student.che.nl>
2025-04-17 11:56:07 +0200

    Small conflict resolve.

--- ProjectGrowthPath.Domain/Entities/LearningToolCompetence.cs
+++ ProjectGrowthPath.Domain/Entities/LearningToolCompetence.cs
@@ -8,11 +8,7 @@ namespace ProjectGrowthPath.Domain.Entities
 {
     public class LearningToolCompetence
     {
-<<<<<<< HEAD:ProjectGrowthPath.Domain/Entities/ToolCompetence.cs
-        public int ToolCompID { get; set; }
-=======
         public int LearningToolCompID { get; set; }
->>>>>>> e65e385defb07e54630a21a4f7603355db8b0046:ProjectGrowthPath.Domain/Entities/LearningToolCompetence.cs
         public int LearningToolID { get; set; }
         public int CompetenceID { get; set; }
 
