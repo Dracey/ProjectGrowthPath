3de2a3473372a47795e0d84daba5ab516f7983d0
Maria Maas <meagmaas@student.che.nl>
2025-05-13 14:32:27 +0200

    Added a new service to help with mutial functions in the learingtoolsetup

---
+++ ProjectGrowthPath.Application/Service/LearningToolSetupHelper.cs
@@ -0,0 +1,37 @@
+﻿using ProjectGrowthPath.Application.Interfaces.IServices;
+using ProjectGrowthPath.Domain.Enums.LearningTools;
+
+namespace ProjectGrowthPath.Application.Service;
+public class LearningToolSetupHelper : ILearningToolSetupHelper
+{
+    public string GetDifficultyLabel(DifficultyTool difficulty)
+    {
+        return difficulty switch
+        {
+            DifficultyTool.Easy => "Gemakkelijk",
+            DifficultyTool.Medium => "Gemiddeld",
+            DifficultyTool.Hard => "Moeilijk",
+            _ => "Onbekend"
+        };
+    }
+
+    public string GetCategoryOfTool(CategoryTool category)
+    {
+        return category switch
+        {
+            CategoryTool.Article => "Artikel",
+            CategoryTool.Book => "Boek",
+            CategoryTool.Course => "Cursus",
+            CategoryTool.Game => "Spel",
+            CategoryTool.Video => "Video",
+            CategoryTool.Podcast => "Podcast",
+            CategoryTool.Other => "Overig"
+        };
+    }
+
+    public string GetImagePath(CategoryTool category)
+    {
+        var fileName = category.ToString().ToLower() + ".png"; // "article.png", etc.
+        return $"tools-images/{fileName}";
+    }
+}
